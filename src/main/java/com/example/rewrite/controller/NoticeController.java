efba3ae2f67b176b9b773ec423f378f68b0ead57
TAEWON KO <migmaknoob@gmail.com>
2025-04-15 21:36:23 +0900

    Noticepage Pageable

--- src/main/java/com/example/rewrite/controller/NoticeController.java
+++ src/main/java/com/example/rewrite/controller/NoticeController.java
@@ -3,7 +3,12 @@ package com.example.rewrite.controller;
 import com.example.rewrite.entity.Notice;
 import com.example.rewrite.repository.Notice.NoticeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -18,18 +23,31 @@ public class NoticeController {
     @Autowired
     private NoticeRepository noticeRepository;
 
-    @GetMapping("/notice")
-    public String notice(HttpSession session, RedirectAttributes redirectAttributes) {
+    @GetMapping("/noticeList")
+    public String notice(@PageableDefault(sort = "noticeId", direction = Sort.Direction.DESC) Pageable pageable, Model model) {
+        Page<Notice> noticePage = noticeRepository.findAll(pageable); // 페이지에이블
+        model.addAttribute("noticePage", noticePage);
 
-        List<Notice> notices = noticeRepository.findAll();
-        redirectAttributes.addFlashAttribute("notices", notices);
+        // 페이지 블록 계산 - 10개씩
+        int pageBlockSize = 10;
+        int pageNow = noticePage.getPageable().getPageNumber() + 1; // 현재 페이지
+        int totalPages = noticePage.getTotalPages();
 
-        return "noticeList";
+        // 시작, 끝 계산기
+        int startIdx = ((pageNow - 1) / pageBlockSize) * pageBlockSize + 1;
+        int endIdx = Math.min(startIdx + pageBlockSize - 1, totalPages);
+
+        model.addAttribute("startIdx", startIdx);
+        model.addAttribute("endIdx", endIdx);
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("pageNow", pageNow);
+
+        return "notice/noticeList";
     }
 
     @GetMapping("/noticeDetail")
     public String noticeDetail(HttpSession session, RedirectAttributes redirectAttributes) {
 
-        return "user/noticeDetail";
+        return "notice/noticeDetail";
     }
 }
