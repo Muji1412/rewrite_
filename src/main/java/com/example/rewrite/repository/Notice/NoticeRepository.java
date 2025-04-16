334f42d446457496d83c8b727735cfb109eb9460
KO TAEWON <migmaknoob@gmail.com>
2025-04-16 17:48:16 +0900

    로그인 기능 구현중 - 스프링 시큐리티?

--- src/main/java/com/example/rewrite/repository/Notice/NoticeRepository.java
+++ src/main/java/com/example/rewrite/repository/Notice/NoticeRepository.java
@@ -3,7 +3,9 @@ package com.example.rewrite.repository.Notice;
 import com.example.rewrite.command.NoticeDTO;
 import com.example.rewrite.entity.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long>, NoticeCustomRepository {
 
     Notice getNoticeByNoticeId(long noticeId);
