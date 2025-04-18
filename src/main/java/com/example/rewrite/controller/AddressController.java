4cde12122513660db09b23664494496270c7e8f1
kimhyeongseob <hs8316@gmail.com>
2025-04-18 17:20:47 +0900

    Add address detail page and Google Cloud Storage configuration

--- src/main/java/com/example/rewrite/controller/AddressController.java
+++ src/main/java/com/example/rewrite/controller/AddressController.java
@@ -1,11 +1,13 @@
 package com.example.rewrite.controller;
 
 import com.example.rewrite.entity.Address;
+import com.example.rewrite.repository.address.AddressRepository;
 import com.example.rewrite.service.address.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,6 +32,23 @@ public class AddressController {
     @GetMapping("/reg") //주소지 등록 페이지
     public String addressReg(){
         return "address/addressWrite";
+    }
+
+    @PostMapping("/write")
+    public String addressWrite(Address address,
+                               @RequestParam("postcode")String postcode,
+                               @RequestParam("addr")String addr,
+                               @RequestParam("detailAddress")String addressDetail,
+                               @RequestParam("phone1")String phone1,
+                               @RequestParam("phone2")String phone2,
+                               @RequestParam("phone3")String phone3
+                               ){
+        address.setAddress(postcode+"/"+addr+"/"+addressDetail);
+        address.setPhoneNum(phone1 + phone2 + phone3);
+        System.out.println(address.toString());
+        addressService.addressWrite(address);
+
+        return "redirect:/address/detail";
     }
 
     @GetMapping("/edit") //주소지 수정 페이지
