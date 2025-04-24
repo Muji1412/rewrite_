008284a1fa889fa317b590f5a2028ed561df1b7a
kimyoonjoo <kimyoonjoo20@gmail.com>
2025-04-24 16:18:36 +0900

    배송상품 데이터 띄우기

---
+++ src/main/java/com/example/rewrite/entity/Cart.java
@@ -0,0 +1,34 @@
+package com.example.rewrite.entity;
+
+import lombok.*;
+import javax.persistence.*;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(
+        name = "cart",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"uid", "prod_id"})
+)
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private Long cartId; // 단일 PK
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "uid", nullable = false)
+    private Users user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "prod_id", nullable = false)
+    private Product product;
+
+    @Column(name = "is_checked",  columnDefinition = "boolean default false")
+    private Boolean isChecked = false;
+}
