4541e743210c0fb830a7389c63004e8899ea5b17
KO TAEWON <migmaknoob@gmail.com>
2025-04-29 12:32:17 +0900

    test

--- src/test/java/com/example/rewrite/NoticeTest/NoticeTest.java
+++ src/test/java/com/example/rewrite/NoticeTest/NoticeTest.java
@@ -1,5 +1,6 @@
 package com.example.rewrite.NoticeTest;
 
+import com.example.rewrite.command.user.SignupRequestDto;
 import com.example.rewrite.entity.Notice;
 import com.example.rewrite.entity.Product;
 import com.example.rewrite.entity.Users;
@@ -9,11 +10,13 @@ import com.example.rewrite.repository.product.ProductRepository;
 import com.example.rewrite.repository.users.UsersRepository;
 import com.example.rewrite.service.cart.CartService;
 import com.example.rewrite.service.mail.MailService;
+import com.example.rewrite.service.user.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
@@ -22,10 +25,8 @@ public class NoticeTest {
     @Autowired
     MailService mailService;
 
-    @Test
-    public void test() {
-        mailService.sendWelcomeEmail("imurditto@gmail.com","김직댕","익맴댕");
-    }
+    @Autowired
+    UserService userService;
 
 
 }
